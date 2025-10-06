3a5f34a6b31af999578e31c63ef1152ffee64966
Alejandro Ordaz <oordaz@nrtec.com.mx>
2025-10-06 08:21:55 -0600

    alert_insert

--- lib/sqlite.dart
+++ lib/sqlite.dart
@@ -15,12 +15,55 @@ class _SQLiteState extends State<SQLite> {
         children: [
           ElevatedButton(
             onPressed: () {
-
+              _insert(context);
             },
             child: Text('Agregar datos'),
           )
         ],
       ),
     );
+  }
+
+  // Button onPressed methods
+  void _insert(BuildContext context) async {
+    final name = TextEditingController();
+    final age = TextEditingController();
+
+    showDialog(
+        context: context,
+        builder: (BuildContext context) {
+      return AlertDialog(
+          title: const Text('Insertar nuevo usuario'),
+    content: Column(
+      children: [
+
+      ],
+    ),
+        actions: <Widget>[
+          TextButton(
+            onPressed: () {
+              Navigator.of(context).pop(); // Cerrar el AlertDialog
+            },
+            child: const Text('Cancelar'),
+          ),
+          TextButton(
+            onPressed: () async {
+              ///JSON
+              Map<String, dynamic> row = {
+                DatabaseHelper.columnName: name.text,
+                DatabaseHelper.columnAge: int.parse(age.text)
+              };
+              final id = await dbHelper.insert(row);
+
+              debugPrint('inserted row id: $id'); // = print("");
+
+              Navigator.of(context).pop(); // Cerrar el AlertDialog
+            },
+            child: const Text('Guardar'),
+          ),
+        ],
+
+      );
+    )
   }
 }
